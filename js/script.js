4faea23b8b62851be0db6346cdda4310af8c769c
unknown <m.radhes@gmail.com>
2020-07-31 18:00:19 +0530

    new updates

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
 function editStatus() {
 	// body...
-	window.open("edit-menu-item-status.html")
+	var a = document.getElementById('title').value;
+	var b = document.getElementById('price').value;
+	var c = document.getElementById('date').value;
+	if (a.length>1 && a.length<66) {
+		if (isNaN(b)) {
+			alert('price should be number');
+			
+
+		}
+		else{
+			var dattee = Date.parse(c)
+			if (isNaN(dattee)) {
+				alert('date format wrong')
+				
+			}
+			else{
+				window.open("edit-menu-item-status.html");	
+			}
+			
+		}
+	}
+	else{
+		alert('Name length check');
+	}
+	return false();
 }
+
